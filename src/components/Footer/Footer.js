3bff9d0cbb85ebd7e85bcc139abf66962b1965c2
zamir0707z <zamir0707z>
2023-10-30 11:35:46 +0300

    aa

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.scss'
+
+const Footer = () => {
+    return (
+        <div id='footer'>
+            <div className="container">
+                <div className='footer'>
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nostrum!</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.scss'
+
+const Header = () => {
+    return (
+        <div id='header'>
+            <div className="container">
+                <div className='header'>
+                   <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, expedita!</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
