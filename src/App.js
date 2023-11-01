1a60b04b2a6b10df57b87911f0c78d5f79ce927e
mamatzanov0707 <115866114+mamatzanov0707@users.noreply.github.com>
2023-11-01 12:27:41 +0300

    za (#1)
    Co-authored-by: zamir0707z <zamir0707z>

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.scss';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import {Route, Routes} from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Block from "./Block/Block";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Routes>
-          {/*<Route path={'/'} element={}/>*/}
+          <Route path={'/'} element={<Block/>}/>
       </Routes>
       <Footer/>
     </div>
---
+++ src/Block/Block.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Exception from "../page/Exception/Exception";
+import Happy from "../page/Happy/Happy";
+
+const Block = () => {
+    return (
+        <div>
+            <Exception/>
+            <Happy/>
+        </div>
+    );
+};
+
+export default Block;
