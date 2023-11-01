584d9bf2a08d8f58a9d47296435410b5a2e2dfa8
janettadev <138752707+janettadev@users.noreply.github.com>
2023-11-01 16:47:51 +0600

    . (#2)

---
+++ src/page/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hero.scss";
+
+const Hero = () => {
+  return (
+    <div id="hero">
+      <div className="container">
+        <div className="hero">
+            <h3>fghjhghjjh</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
