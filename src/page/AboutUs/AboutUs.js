962a6a37d9e61e62d0aa8157db42bfb430d8aab4
janettadev <138752707+janettadev@users.noreply.github.com>
2023-11-04 13:28:37 +0600

    header (#4)

---
+++ src/page/AboutUs/AboutUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./AboutUs.scss";
+
+const AboutUs = () => {
+  return (
+    <div id="aboutUs">
+      <div className="constainer">
+        <div className="aboutUs"></div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
